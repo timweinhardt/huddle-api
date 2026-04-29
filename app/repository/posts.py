6b31a89ac2fbba42566e416de51a43b33259ef9e
timweinhardt <timothyweinhardt@gmail.com>
2026-04-29 17:50:50 -0400

    Rename to PostRepository

--- app/repository/posts.py
+++ app/repository/posts.py
@@ -1,6 +1,6 @@
 from app.core.config import config
 from app.db import Table
 
-class PostService:
+class PostRepository:
     def __init__(self):
         self.db = Table(config.posts_table_name, config.aws_region)
